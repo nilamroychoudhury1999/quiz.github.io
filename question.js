f89bef37b7614ffb849f60e38fd3cc67c2efcc19
Nilamroychoudhury1999 <63612266+nilamroychoudhury1999@users.noreply.github.com>
2021-05-10 16:33:54 +0530

    Add files via upload

---
+++ question.js
@@ -0,0 +1,43 @@
+var questions = [{
+    "question": "Grand Central Terminal, Park Avenue, New York is the world's",
+    "option1": "largest railway station",
+    "option2": "highest railway station",
+    "option3": "longest railway station",
+    "option4": "None of the above",
+    "answer": "1"
+}, {
+    "question": "Entomology is science that studies",
+    "option1": "Behaviour of human beings",
+    "option2": "insects",
+    "option3": "the origin and history of technical and sc terms",
+    "option4": "formation of rocks",
+    "answer": "2"
+}, {
+    "question": "Eritrea, which became the 182nd member of the UN in 1993, is in the  continent of",
+    "option1": "asia",
+    "option2": "africa",
+    "option3": "europe",
+    "option4": "aust",
+    "answer": "2"
+}, {
+    "question": "Garampani sanctuary is located at",
+    "option1": "junagarh",
+    "option2": "diphu",
+    "option3": "kohima",
+    "option4": "gangtok",
+    "answer": "2"
+}, {
+    "question": "For which of the following disciplines is Nobel Prize awarded?",
+    "option1": "phy and chem",
+    "option2": "physio and med",
+    "option3": "literature",
+    "option4": "All",
+    "answer": "4"
+}, {
+    "question": "Hitler party which came into power in 1933 is known as",
+    "option1": "labour ",
+    "option2": "nazi",
+    "option3": "ku klux",
+    "option4": "democratic",
+    "answer": "2"
+}];
